065976823847c238906387127340bcc929116a6c
Muneeb <muhammadmuneebulraza@gmail.com>
2024-07-28 04:11:17 -0700

    Fetch api data and comment Parent Commopent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import FetchApi from "./Component/FetchApi";
 import Parent from "./Component/Parent";
 
 export default function App() {
   return (
     <div className="text-3xl  font-bold m-2">
-     <p className="text-center text-blue-600 mb-20 text-4xl"> practice of LIFTING STATE</p>
-      <Parent/>
+     {/* <p className="text-center text-blue-600 mb-20 text-4xl"> practice of LIFTING STATE</p>
+      <Parent/> */}
+      <FetchApi/>
     </div>
   )
 }
